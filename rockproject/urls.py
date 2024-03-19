c36e6a8e86d380d317edfdb40113de41b5377046
Eric Steedly <ericsteedly@gmail.com>
2024-03-19 12:02:28 -0500

    added type URL

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import include
 from rest_framework import routers
-from rockapi.views import register_user, login_user
+from rockapi.views import register_user, login_user, TypeView
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'types', TypeView, 'type')
 
 urlpatterns = [
     path('', include(router.urls)),
